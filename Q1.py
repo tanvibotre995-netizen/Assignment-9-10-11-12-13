3767ce0df1fc033efe90b58e562bc40fe57a8751
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-06-29 21:13:22 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,10 @@
+#write a program which contains one function named as Display() that prints "Jay Ganesh" on console.
+
+def Display():
+    print("Jay Ganesh")
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
