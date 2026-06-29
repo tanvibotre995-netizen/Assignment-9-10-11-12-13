2c3d46e484854a8ddb14ffb0005923f183065d96
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-06-30 00:27:16 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,12 @@
+def ChkGreater(a,b):
+    if a > b:
+        print(a,"is greater")
+    else:
+        print(b,"is greater")
+
+def main():
+    ChkGreater(10,20)
+    
+    
+if __name__ == "__main__":
+    main()
