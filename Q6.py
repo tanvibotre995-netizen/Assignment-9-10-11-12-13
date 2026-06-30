75e4deac75b3f93488280f27788ca0b2c2a150dc
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-06-30 14:03:50 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,16 @@
+def Multiplication_table(n):
+    for i in range(1,11):
+        result = n * i
+        print(result)
+
+def main():
+    n=int(input("enter the number:"))
+    Multiplication_table(n)
+    
+    
+    
+if __name__ == "__main__":
+    main()
+
+
+        
