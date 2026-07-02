3f56dea5283901a7c6080d3429d474fb4543a8a4
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-02 14:12:59 +0530

    Create Q25.py

---
+++ Q25.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 75:
+    print("Distinction")
+elif marks >= 60:
+    print("First Class")
+elif marks >= 50:
+    print("Second Class")
+else:
+    print("Fail")
