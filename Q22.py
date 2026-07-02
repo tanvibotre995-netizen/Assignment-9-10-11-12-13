2d80d3a21a2fe136434226e83e216699d4107a15
tanvibotre995-netizen <tanvibotre995@gmail.com>
2026-07-02 13:53:01 +0530

    Create Q22.py

---
+++ Q22.py
@@ -0,0 +1,11 @@
+def reactangle(l,w):
+    area=l * w
+    print("area of reactangle is",area)
+
+def main():
+    Value1=int(input("enter number:"))
+    Value2=int(input("enter number:"))
+    reactangle(Value1,Value2)
+
+if __name__ == "__main__":
+    main()
